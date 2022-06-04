753c592edd38944fcefd4cf3d8bba4b17ad47bed
lriley2020 <liam.j.06.riley@gmail.com>
2022-06-04 10:53:20 +0100

    Added symlink script

---
+++ makesymlink.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Warning: this script assumes you have cloned the repository into your home directory and that you have a ~/bin folder in your $PATH"
+echo "If either of these do not apply to you, you may need to change this script""
+read -p "Do you wish to continue?(y/n)"
+if [ "$REPLY" != "y" ]; then
+   exit
+fi
+ln -s /home/$USER/bin/ifttt_message /home/$USER/ifttt_message/ifttt_message
