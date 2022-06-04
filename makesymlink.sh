c090a23bde2e98586858b2f7978bf9093d8b46b0
lriley2020 <liam.j.06.riley@gmail.com>
2022-06-04 11:17:45 +0100

    Fixed broken script

--- makesymlink.sh
+++ makesymlink.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 echo "Warning: this script assumes you have cloned the repository into your home directory and that you have a ~/bin folder in your PATH"
-echo "If either of these do not apply to you, you may need to change this script""
+echo "If either of these do not apply to you, you may need to change this script"
 read -p "Do you wish to continue?"
 if [ "$REPLY" != "y" ]; then
    exit
